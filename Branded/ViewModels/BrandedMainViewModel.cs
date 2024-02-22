467d484bbdf6505385a905303120ba2ae14dd6c1
Sanjay Govind <sanjay.govind9@gmail.com>
2024-02-22 13:22:59 +1300

    Instead of search the device tree twice, just store the found device and use that

--- Branded/ViewModels/BrandedMainViewModel.cs
+++ Branded/ViewModels/BrandedMainViewModel.cs
@@ -10,17 +10,11 @@ namespace SantrollerConfiguratorBranded.NetCore.ViewModels;
 
 public partial class BrandedMainViewModel : ReactiveObject, IRoutableViewModel
 {
-    private readonly HashSet<string> _validDevices = new();
     public BrandedMainViewModel(BrandedMainWindowViewModel screen)
     {
         BrandedMain = screen;
         HostScreen = screen;
-        _validDevices.UnionWith(BrandedMain.Config.Configurations.SelectMany(s => s.Configurations).Select(s => $"{s.VendorName}_{s.ProductName}"));
-        this.WhenAnyValue(s => s.BrandedMain.SelectedDevice).Select(s =>
-                s is Santroller santroller && _validDevices.Contains($"{santroller.Manufacturer}_{santroller.Product}"))
-            .ToPropertyEx(this, x => x.Configurable);
     }
-    [ObservableAsProperty] public bool Configurable { get; }
     public BrandedMainWindowViewModel BrandedMain { get; }
 
     public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
--- Branded/ViewModels/BrandedMainWindowViewModel.cs
+++ Branded/ViewModels/BrandedMainWindowViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Reactive.Linq;
@@ -20,6 +21,7 @@ public partial class BrandedMainWindowViewModel : MainWindowViewModel
 {
     public BrandedConfigurationStore Config { get; }
 
+    [ObservableAsProperty] public BrandedConfiguration? SelectedDeviceConfig { get; set; }
     [Reactive] public BrandedConfigurationSection SelectedSection { get; set; }
     [Reactive] public BrandedConfiguration SelectedConfig { get; set; }
 
@@ -27,11 +29,13 @@ public partial class BrandedMainWindowViewModel : MainWindowViewModel
 
     private TaskCompletionSource<string>? _bootloaderPath;
 
-    [ObservableAsProperty] public bool ReadyToConfigureBranded { get; }
+    [ObservableAsProperty] public bool ReadyToConfigureBranded { get; set; }
 
     private bool _writing;
 
     public bool HasMultipleConfigs => Config.Configurations.Count > 1;
+
+    public Dictionary<string, BrandedConfiguration> ValidConfigurations { get; } 
 
     [RelayCommand]
     public async Task LoadSelectedConfig()
@@ -49,6 +53,7 @@ public partial class BrandedMainWindowViewModel : MainWindowViewModel
     public BrandedMainWindowViewModel() : base(true)
     {
         Config = BrandedConfigurationStore.LoadBranding(this);
+        ValidConfigurations = Config.Configurations.SelectMany(s => s.Configurations).ToDictionary(s => $"{s.VendorName}_{s.ProductName}");
         Logo = Config.Logo;
         ProgressBarPrimary = ColorToHex(Config.PrimaryColor);
         ProgressBarWarning = ColorToHex(Config.WarningColor);
@@ -76,6 +81,8 @@ public partial class BrandedMainWindowViewModel : MainWindowViewModel
                 s is {Item1: not null, Item2: true} &&
                 (!s.Item3 || s.Item4 == null))
             .ToPropertyEx(this, s => s.ReadyToConfigureBranded);
+        this.WhenAnyValue(x => x.SelectedDevice).Select(s => s is not Santroller santroller ? null : ValidConfigurations.GetValueOrDefault($"{santroller.Manufacturer}_{santroller.Product}"))
+            .ToPropertyEx(this, x => x.SelectedDeviceConfig);
     }
 
     public void DeviceAdded(IConfigurableDevice device)
@@ -159,60 +166,18 @@ public partial class BrandedMainWindowViewModel : MainWindowViewModel
             Windows ? "Waiting for device (Stuck here? Try clicking refresh devices)" : "Waiting for device", null);
     }
 
-    public void test1()
-    {
-        if (SelectedDevice is not Santroller santroller) return;
-        SelectedConfig = Config.Configurations.SelectMany(s => s.Configurations).First(s =>
-            s.VendorName == santroller.Manufacturer && s.ProductName == santroller.Product);
-    }
-
-    public void test2()
-    {
-        if (SelectedDevice is not Santroller santroller) return;
-        Model = new ConfigViewModel(this, SelectedDevice, true);
-    }
-
-    public void test3()
-    {
-        if (Model == null) return;
-        new SerializedConfiguration(SelectedConfig.Model).LoadConfiguration(Model);
-    }
-
-    public void test4()
-    {
-        if (Model == null) return;
-        Model.UpdateBluetoothAddress();
-    }
-
-    public void test5()
-    {
-        if (SelectedDevice is not Santroller santroller) return;
-        if (Model == null) return;
-        santroller.LoadConfiguration(Model, true);
-    }
-
-    public void test6()
-    {
-        if (Model == null) return;
-        Router.Navigate.Execute(Model);
-    }
-
-    public void test7()
-    {
-        if (Model == null) return;
-        Model.SetUpDiff();
-    }
     [RelayCommand]
     public void ConfigureBranded()
     {
-        if (SelectedDevice is not Santroller) return;
-        test1();
-        test2();
-        test3();
-        test4();
-        test5();
-        test6();
-        test7();
+        if (SelectedDevice is not Santroller santroller || SelectedDeviceConfig == null) return;
+        SelectedConfig = SelectedDeviceConfig;
+        // Seems there are cases where selectedconfig can be null. Why is that?
+        Model = new ConfigViewModel(this, SelectedDevice, true);
+        new SerializedConfiguration(SelectedConfig.Model).LoadConfiguration(Model);
+        Model.UpdateBluetoothAddress();
+        santroller.LoadConfiguration(Model, true);
+        Router.Navigate.Execute(Model);
+        Model.SetUpDiff();
     }
 
     [RelayCommand]
