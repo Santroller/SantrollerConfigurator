3a1f2c8d933d6680de3cc9309b6054e0b747804e
Sanjay Govind <sanjay.govind9@gmail.com>
2026-05-17 13:36:53 +1200

    fix gha tilt

--- santroller-configurator/Configuration/Outputs/GuitarAxis.cs
+++ santroller-configurator/Configuration/Outputs/GuitarAxis.cs
@@ -354,7 +354,7 @@ public partial class GuitarAxis : OutputAxis
         
         if (mode is not (ConfigField.Ps3 or ConfigField.Ps2 or ConfigField.Ps3WithoutCapture or ConfigField.Ps4 or ConfigField.Xbox360
             or ConfigField.XboxOne
-            or ConfigField.Universal or ConfigField.Xbox or ConfigField.Wii)) return "";
+            or ConfigField.Universal or ConfigField.Xbox or ConfigField.Wii or ConfigField.Arcade)) return "";
         // The below is a mess... but essentially we have to handle converting the input to its respective output depending on console
         // We have to do some hyper specific stuff for digital to analog here too so its easiest to capture its value once
         var analogOn = 0;
