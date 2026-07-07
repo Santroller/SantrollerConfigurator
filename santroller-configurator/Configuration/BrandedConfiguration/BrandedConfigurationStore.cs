dce5c56256762a0a2a25b7f404f28cc6fa1dbf9f
Sanjay Govind <sanjay.govind9@gmail.com>
2026-07-07 12:25:40 +1200

    work on fixing builder

--- santroller-configurator/Configuration/BrandedConfiguration/BrandedConfigurationStore.cs
+++ santroller-configurator/Configuration/BrandedConfiguration/BrandedConfigurationStore.cs
@@ -88,7 +88,7 @@ public partial class BrandedConfigurationStore : ReactiveObject
     public static BrandedConfigurationStore LoadBranding(MainWindowViewModel model)
     {
 #if Windows && SINGLE_FILE
-        var path = Path.Join(AppContext.BaseDirectory, "branding.bin");
+        var path = Path.Join(AppContext.GetData("NATIVE_DLL_SEARCH_DIRECTORIES").ToString(), "branding.bin");
         if (!File.Exists(path))
         {
             path = Path.Join(Path.GetDirectoryName(Path.GetDirectoryName(Environment.ProcessPath)!)!, "Resources",
